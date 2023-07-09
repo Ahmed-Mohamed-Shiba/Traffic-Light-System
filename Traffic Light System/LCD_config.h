3ad334ad7b303e673a01320792eb4b78ec051fb2
Ahmed-Mohamed-Shiba <121357837+Ahmed-Mohamed-Shiba@users.noreply.github.com>
2023-07-09 22:41:39 +0300

    Add files via upload

---
+++ Traffic Light System/LCD_config.h
@@ -0,0 +1,12 @@
+/*
+ * LCD_config.h
+ *
+ *  Created on: Jul 6, 2022
+ *      Author: Ahmed Shiba
+ */ 
+
+
+#ifndef LCD_CONFIG_H_
+#define LCD_CONFIG_H_
+#define eight_bits_mode
+#endif /* LCD_CONFIG_H_ */
